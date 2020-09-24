c93757a78f033c6776906988fc5e080c58015ceb
qtzx94 <qtzx94@gmail.com>
2020-09-24 16:24:56 +0800

    半双工通信

--- net/client.js
+++ net/client.js
@@ -1,44 +1,51 @@
-  const net = require('net')
+const net = require('net')
+const { join } = require('path')
 
-  const socket = new net.Socket({})
+const socket = new net.Socket({})
 
-  socket.connect({
-    host: '127.0.0.1',
-    port: 4000
-  })
+socket.connect({
+  host: '127.0.0.1',
+  port: 4000
+})
 
-  // socket.write('good morning geekbang') // 单工通信，只能单向传输数据
+const lessonids = [
+  "136797",
+  "136798",
+  "136799",
+  "136800",
+  "136801",
+  "136803",
+  "136804",
+  "136806",
+  "136807",
+  "136808",
+  "136809",
+  "141994",
+  "143517",
+  "143557",
+  "143564",
+  "143644",
+  "146470",
+  "146569",
+  "146582"
+]
 
-  const lessonids = [
-    '123451', '123452', '123453', '123454'
-  ]
+let id = Math.floor(Math.random() * lessonids.length)
 
-  let buffer = Buffer.alloc(4)
-  let id = Math.floor(Math.random() * lessonids.length)
-  buffer.writeInt32BE(
-    lessonids[id]
-  )
+// 往服务器传数据
+socket.write(encode(id))
 
+socket.on('data', buffer => {
+  console.log(buffer.toString())
 
-  socket.on('data', (buffer) => {
-    seqBuffer = buffer.slice(0, 2)
-    titleBuffer = buffer.slice(2)
+  // 接收到数据之后，按照半双工通信的逻辑，马上开始下一次请求
+  id = Math.floor(Math.random() * lessonids.length)
+  socket.write(encode(id))
+})
 
-    console.log(seqBuffer.readInt16BE(), titleBuffer.toString())
-
-    id = Math.floor(Math.random() * lessonids.length)
-
-    socket.write(encode(id))
-
-  })
-
-  let seq = 0
-  function encode(index) {
-    buffer = Buffer.alloc(6)
-    buffer.writeInt16BE(seq)
-    buffer.writeInt32BE(lessonids[index], 2)
-
-    console.log(seq, lessonids[index])
-    seq++
-    return buffer
-  }
+// 把编码请求包的逻辑封装为一个函数
+function encode(index) {
+  buffer = Buffer.alloc(4) // 创建一个长度为4的buffer
+  buffer.writeInt32BE(lessonids[index])
+  return buffer
+}
