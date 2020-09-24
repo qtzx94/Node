7279e5d6cd1ae7a3862a89e1b2b2d27d5811de3e
qtzx94 <qtzx94@gmail.com>
2020-09-24 15:42:07 +0800

    全双工通信

--- net/client.js
+++ net/client.js
@@ -7,4 +7,38 @@
     port: 4000
   })
 
-  socket.write('good morning geekbang')
+  // socket.write('good morning geekbang') // 单工通信，只能单向传输数据
+
+  const lessonids = [
+    '123451', '123452', '123453', '123454'
+  ]
+
+  let buffer = Buffer.alloc(4)
+  let id = Math.floor(Math.random() * lessonids.length)
+  buffer.writeInt32BE(
+    lessonids[id]
+  )
+
+
+  socket.on('data', (buffer) => {
+    seqBuffer = buffer.slice(0, 2)
+    titleBuffer = buffer.slice(2)
+
+    console.log(seqBuffer.readInt16BE(), titleBuffer.toString())
+
+    id = Math.floor(Math.random() * lessonids.length)
+
+    socket.write(encode(id))
+
+  })
+
+  let seq = 0
+  function encode(index) {
+    buffer = Buffer.alloc(6)
+    buffer.writeInt16BE(seq)
+    buffer.writeInt32BE(lessonids[index], 2)
+
+    console.log(seq, lessonids[index])
+    seq++
+    return buffer
+  }
--- net/server.js
+++ net/server.js
@@ -1,10 +1,23 @@
 const net = require('net')
 
-const server = net.createServer(socket => {
-  // socket.write() // 写入
-  socket.on('data', function(buffer) {
-    console.log(buffer, buffer.toString())
-  }) // 监听
+const server = net.createServer((socket) => {
+
+  server.on('data', (buffer) => {
+    const seqBuffer = buffer.slice(0, 2)
+    const lessonid = buffer.readInt32BE(2)
+
+    setTimeout(() => {
+      const buffer = Buffer.concat([seqBuffer, Buffer.from(data[lessonid])])
+      socket.write(buffer)
+    }, 500)
+  })
 }) 
 
 server.listen(4000)
+
+const data = {
+  12341: '01 | 课程介绍',
+  12342: '02 | 内容综述',
+  12343: '03 | Node.js 是什么',
+  12344: '04 | Node.js 可以用来做什么'
+}
